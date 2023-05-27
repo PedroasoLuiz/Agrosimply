9946de595857fec4c1a58a6a912fb265ad37995a
Pedro Luiz <pedroluiz9922@gmail.com>
2023-05-27 17:58:54 -0300

    Integração

--- AgroSimply_V2/AgroSimply_V2/Program.cs
+++ AgroSimply_V2/AgroSimply_V2/Program.cs
@@ -1,62 +1,95 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using AgroSimply_V2.Data;
-using AgroSimply_V2;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AgroSimply_V2Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AgroSimply_V2Context") ?? throw new InvalidOperationException("Connection string 'AgroSimply_V2Context' not found.")));
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseRouting();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
 
-
-
-app.MapPropriedadeEndpoints();
-
-app.MapTelefoneEndpoints();
-
-app.MapUsuarioEndpoints();
-
-app.MapProdutorEndpoints();
-
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
+
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using AgroSimply_V2.Data;
+//using AgroSimply_V2;
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<AgroSimply_V2Context>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("AgroSimply_V2Context") ?? throw new InvalidOperationException("Connection string 'AgroSimply_V2Context' not found.")));
+
+//// Add services to the container.
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+
+
+//app.MapPropriedadeEndpoints();
+
+//app.MapTelefoneEndpoints();
+
+//app.MapUsuarioEndpoints();
+
+//app.MapProdutorEndpoints();
+
+
+
+//app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
